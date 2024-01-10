0ffab24b22fa24d9cd2f43892692136fe262c662
Chhay2 <117246850+Chhay2@users.noreply.github.com>
2024-01-10 10:45:51 +0700

    retry adding

--- src/main/java/se/project/add_to_cart_project/Model/Registration.java
+++ src/main/java/se/project/add_to_cart_project/Model/Registration.java
@@ -2,6 +2,9 @@ package se.project.add_to_cart_project.Model;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 
 @Controller
@@ -9,6 +12,19 @@ public class Registration {
     @GetMapping("/Registration")
     public String registrationpage(){
         return "Registration";
+    }
+    @GetMapping("/buylist")
+    public String BuyListpage() {
+        return "BuyList";
+    }
+
+    @GetMapping("/Insert")
+    public String product() {
+        return "InsertPage";
+    }
+    @GetMapping("/Payment")
+    public String Payment() {
+        return "PaymentPage";
     }
     
 }
